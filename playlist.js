74131fa93bc090c08a4d7a862828973233fa577c
Paulo Henrique <gladgamer01@gmail.com>
2018-07-20 09:17:25 -0300

    now you can view share link

--- playlist.js
+++ playlist.js
@@ -3,7 +3,24 @@ var x = JSON.parse(decodeURI(window.location.search.substr(1)));
 }
 catch {}
 
-adicionarVideo = (link,playlist) => {
+playlists = [
+
+]
+
+$("#urlplaylist").text(window.location.href);
+
+
+adicionarVideo = (link,playlist,type) => {
+
+	if(playlists.length >0){
+	save = 0;
+	for(x=0;x<playlists.length;x++){
+		if(playlists[x].title == playlist){
+			save == x;
+		}
+	}
+}
+
 	if(link === "" || playlist ===""){alert('Porfavor prencha todos campos!');}else{
 	if(link.substr(0,5) == "http:") {
 	id = link.substr(31,link.length);
@@ -14,9 +31,16 @@ adicionarVideo = (link,playlist) => {
 	deletar.setAttribute('id','video' + id + 'btn');
 	document.getElementById('div' + playlist).appendChild(deletar);
 	
+	try{
+	playlists[save].links.push(id);
+	$("#urlplaylist").text(window.location.href + "?" + JSON.stringify(playlists));
+	}catch{}
+
 	$(document).ready(function(){
 		$(document).ready(function(){
 			$("button").click(function(event){
+				
+				$("#urlplaylist").text(window.location.href + "?" + JSON.stringify(playlists));
 					$('#' + event.target.id).remove();
 					$('#' + event.target.id.substr(0,16)).remove();
 			});
@@ -33,9 +57,16 @@ adicionarVideo = (link,playlist) => {
 	deletar.setAttribute('id','video' + id + 'btn');
 	document.getElementById('div' + playlist).appendChild(deletar);
 	
+	try{
+		playlists[save].links.push(id);
+		$("#urlplaylist").text(window.location.href + "?" + JSON.stringify(playlists));
+		}catch{}
+
 	$(document).ready(function(){
 		$(document).ready(function(){
 			$("button").click(function(event){
+			
+				$("#urlplaylist").text(window.location.href + "?" + JSON.stringify(playlists));
 					$('#' + event.target.id).remove();
 					$('#' + event.target.id.substr(0,16)).remove();
 			});
@@ -43,10 +74,14 @@ adicionarVideo = (link,playlist) => {
 });
 
 }
+
 }
+try{
+$("#urlplaylist").text(window.location.href + "?" + JSON.stringify(playlists));
+}catch{}
 }
 
-criarPlaylist = (nome) => {
+criarPlaylist = (nome,type) => {
 	if(nome ==="") {alert('Porfavor prencha o nome da playlist!');}else{
 	var playlist = document.createElement('div');	
 	playlist.id = "div" + nome;
@@ -56,6 +91,9 @@ criarPlaylist = (nome) => {
 	var nameD = document.createTextNode(nome + " Playlist");
 	name.appendChild(nameD);
 	document.getElementById("div" + nome).appendChild(name);
+	if(type == 5){
+	playlists.push({title: nome,links: []});
+	}
 }
 }
 
@@ -68,7 +106,7 @@ deletarPlaylist = () => {
 try {
 for(a=0;a<x.length;a++){
 	if(x[a].title != ""){
-		criarPlaylist(x[a].title);
+		criarPlaylist(x[a].title,0);
 	for(i=0; i< x[a].links.length;i++){
 		adicionarVideo("https://www.youtube.com/watch?v=" + x[a].links[i],x[a].title);
 	}
